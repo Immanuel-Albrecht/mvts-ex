678ab25350b9dff331142341c7ab0f16b696d10a
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-04-20 08:24:47 +0200

    first tool is finished

--- vector time series/ch2/01-unit-root-test-2.8.1.jl
+++ vector time series/ch2/01-unit-root-test-2.8.1.jl
@@ -1,34 +1,37 @@
+#!/usr/bin/env -S julia --sysimage=../sys_vts.so --
+
 using HypothesisTests
 using DataFrames
 using CSV
 using ArgParse
-using Plots
 
 aps = ArgParseSettings(
-    description = "This program does stuff. " * #TODO: add precise description
+    description = "This program performs Augmented Dickey-Fuller tests " * #TODO: add precise description
+                  "on each column of a given data set with increasing " *
+                  "number of self-differences of the series, until " *
+                  "the all differenced series' may be considered unit root free. " *
                   "We suppose that the time series is" *
                   " presented in order from earlier to later," *
                   " and that the intervals between" *
                   " each time point are equal.",
 )
 @add_arg_table! aps begin
-    # "--opt1"
-    #     help = "an option with an argument"
-    # "--opt2", "-o"
-    #     help = "another option with an argument"
-    #     arg_type = Int
-    #     default = 0
-    # "--flag1"
-    #     help = "an option without argument, i.e. a flag"
-    #     action = :store_true
     "--time-column"
-        help = "name of the column containing the time information"
+        help = "name of the column containing the time information, so we can ignore this column"
         arg_type = String
         default = "Period"
     "--p-value"
         help = "p-value for univariate Augmented Dickey-Fuller unit root test"
         arg_type = Real
         default = .01
+    "--lag"
+        help = "lag used to calculate the Augmented Dicker-Fuller unit root test"
+        arg_type = Int
+        default = 1
+    "--store"
+        help = "store the univariate p-value table"
+        arg_type = String
+        required = false
     "csv"
         help = "path to the CSV containing the vector time series"
         required = false
@@ -55,10 +58,40 @@ end
 
 df = CSV.read(args["csv"])
 
+time_col = Symbol(args["time-column"])
+target_p = args["p-value"]
+lag = args["lag"]
 
+dp = Dict(col => Real[] for col in names(df) if ! (col == time_col))
+d_col = Symbol(" D^( ... )")
+dp[d_col] = []
 
-println(names(df))
+println("Accepted p-value <$target_p.")
 
-print( HypothesisTests.ADFTest(df[!, :Automobile], :none, 1) )
+for d in 0:(size(df)[1]-lag)
+    nonzero_count = 0
+    append!(dp[d_col], d)
+    for col in names(df)
+        if ! (col == time_col)
+            y = diff_series(df[!,col],d)
+            p = pvalue(ADFTest(y, :none, lag))
+            significant = (p<target_p) ? " *" : ""
+            append!(dp[col], p)
+            if p >= target_p
+                nonzero_count += 1
+            end
+        end
+    end
+    if nonzero_count < 1
+        break
+    end
+end
 
-println("done.")
+df_p = DataFrame(dp)
+
+println(df_p)
+
+store = args["store"]
+if store != nothing
+    CSV.write(args["store"], df_p)
+end
