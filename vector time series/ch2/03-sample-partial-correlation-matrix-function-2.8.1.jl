1716daacc567aa19efcc01d393fdb59f7f01fbd4
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-06-27 22:56:34 +0200

    working out the details of regressing out linear dependences...

--- vector time series/ch2/03-sample-partial-correlation-matrix-function-2.8.1.jl
+++ vector time series/ch2/03-sample-partial-correlation-matrix-function-2.8.1.jl
@@ -9,6 +9,8 @@ using Distributions
 using GLM
 using Plots
 
+using StatsBase
+
 
 aps = ArgParseSettings(
     description = "This program computes the partial lag correlation matrices for a given " *
@@ -27,7 +29,12 @@ aps = ArgParseSettings(
     "--p-value"
     help = "p-value for significance test whether the cross-correlations are significantly different from zero (based on t-test)"
     arg_type = Real
-    default = 0.01
+    default = 0.025
+    
+    "--fit-p-value"
+    help = "p-value for regression on intermediate series values"
+    arg_type = Real
+    default = .05
     
     "--diff"
     help = "number of times the difference operator D is applied to the input series"
@@ -38,6 +45,21 @@ aps = ArgParseSettings(
     help = "center every observation variable after differencing"
     arg_type = Bool
     default = true
+    
+    "--with-intercept"
+    help = "allow intercept term during regression"
+    arg_type = Bool
+    default = false
+    
+    "--symmetric-choice"
+    help = "use symmetric parameter choice for regression"
+    arg_type = Bool
+    default = false
+    
+    "--force-intercept"
+    help = "force the intercept term into the regression (not a good idea at all)"
+    arg_type = Bool
+    default = false
     
     "--lag-from"
     help = "minimal lag k"
@@ -90,6 +112,57 @@ function center_series(x)
     x .- mean(x)
 end
 
+
+function auto_regression_residuals(X, y, p_value, forced_columns)
+    
+    X_r,X_c = size(X)
+    y_r, = size(y)
+    
+    @assert(X_r == y_r)
+    
+    pre_chosen_columns = Integer[]
+    
+    # add forced columns
+    append!(pre_chosen_columns, unique(forced_columns))
+    
+    if p_value > 0
+        while true
+            ci_pvalues = Float64[]
+            for ci in 1:X_c
+                if ci in pre_chosen_columns
+                    push!(ci_pvalues,2) # ignore this column.
+                    continue
+                end
+                push!(pre_chosen_columns, ci)
+                X_p = X[:,pre_chosen_columns]
+                pop!(pre_chosen_columns)
+                
+                m_p = GLM.fit(LinearModel,X_p,y,true)
+                tb_p = coeftable(m_p)
+                pvalues_p = tb_p.cols[tb_p.pvalcol]
+                n_pvalues, = size(pvalues_p)
+                ci_p = maximum(pvalues_p)
+                push!(ci_pvalues,ci_p)
+            end
+            ci = argmin(ci_pvalues)
+            ci_p = ci_pvalues[ci]
+            
+            if ci_p < p_value
+                # select ci and continue
+                push!(pre_chosen_columns,ci)
+            else
+                break #saturated.
+            end
+        end
+    end
+    
+    X0 = X[:,pre_chosen_columns]
+    m0 = GLM.fit(LinearModel,X0,y,true)
+    
+    return residuals(m0), pre_chosen_columns, m0
+end
+
+
 ###
 
 """
@@ -97,7 +170,13 @@ end
     
     x __ data frame with value columns, rows must be in order of the time column (which must be omitted)
 """
-function calculate_residual_vectors(x, lag::Int)
+function calculate_residual_vectors(x, lag::Int, p_value, with_intercept, force_intercept, symmetric)
+    
+    #
+    # TODO: the problem lies probably here, as we are 'overfitting' thus making
+    #       the vectors very small and the corresponding DU^-1 * CVU * DV^-1 matrices BIG 
+    #
+    
 
     # convert data frame to array ...
     x = Array{Float64}(x)
@@ -114,7 +193,12 @@ function calculate_residual_vectors(x, lag::Int)
         X_cols = n_col * (lag-1)
         
         
-        X = Array{Float64}(undef, rows, X_cols)
+        if with_intercept
+            X = Array{Float64}(undef, rows, X_cols + 1)
+        else
+            X = Array{Float64}(undef, rows, X_cols )
+        end
+        
         
         # fill covariates
         
@@ -124,30 +208,68 @@ function calculate_residual_vectors(x, lag::Int)
             end
         end
         
+        if with_intercept
+            X[:,X_cols + 1] .= 1 # Add intercept term
+        end
+        
         # calculate residuals
         
         U = Array{Float64}(undef, rows, n_col)
         V = Array{Float64}(undef, rows, n_col)
         
+        nU = 0
+        nV = 0
+        
         # each column has to be fitted for themselves
         for c in 1:n_col
                 yu = x[1:rows,c]
                 yv = x[1+lag:lag+rows,c]
-                mu = GLM.fit(LinearModel, X, yu, true)
-                mv = GLM.fit(LinearModel, X, yv, true)
-                yhat_u = predict(mu,X)
-                yhat_v = predict(mv,X)
-                U[:,c] = yu - yhat_u
-                V[:,c] = yv - yhat_v
+                
+                
+                #mu = GLM.fit(LinearModel, X, yu, true)
+                #mv = GLM.fit(LinearModel, X, yv, true)
+                
+                #global X00,y00
+                #X00 = X
+                #y00 = yu
+                
+                #println(coeftable(mu))
+                
+                #yhat_u = predict(mu,X)
+                #yhat_v = predict(mv,X)
+                
+                if force_intercept
+                    chosen_columns = [X_cols + 1]
+                else
+                    chosen_columns = []
+                end
+                
+                residual_u,col_u, = auto_regression_residuals(X,yu,p_value,chosen_columns)
+                residual_v,col_v, = auto_regression_residuals(X,yv,p_value,chosen_columns)
+                
+                if symmetric
+                    
+                    cols = []
+                    append!(cols,col_u)
+                    append!(cols,col_v)
+                    residual_u,col_u, = auto_regression_residuals(X,yu,-1,cols)
+                    residual_v,col_v, = auto_regression_residuals(X,yv,-1,cols)
+                end
+                U[:,c] = residual_u
+                V[:,c] = residual_v
+                nU += length(col_u)
+                nV += length(col_v)
         end
         
-        return U,V
+        return U,V,nU,nV
     
     elseif (lag == 1)
         # for s=1, the formula does not involve any regression at all.
         
         U = Array{Float64}(undef, rows, n_col)
         V = Array{Float64}(undef, rows, n_col)
+        nU = 0
+        nV = 0
         
         # each column has to be fitted for themselves
         for c in 1:n_col
@@ -157,7 +279,7 @@ function calculate_residual_vectors(x, lag::Int)
                 V[:,c] = yv
         end
         
-        return U,V
+        return U,V,nU,nV
         
     end
 end
@@ -194,7 +316,9 @@ function C_UV(U,V,diagonal_inverted::Bool=false)
                 y = V[:,v]
                 if diagonal_inverted
                     variance = mean(x .* y)
-                    C[u,v] = 1/(sqrt(variance))
+                    C[u,v] = 1/(sqrt(variance)) # those guys tend to get very big when there is no more variance after
+                                                # removing the linear effects of intermediate time series values.
+                                                # So, when do we cut this off? And how?
                 else
                     C[u,v] = mean(x .* y)
                 end
@@ -283,15 +407,25 @@ time_col = Symbol(args["time-column"])
 apply_D_count = args["diff"]
 
 p_value = args["p-value"]
+fit_p_value = args["fit-p-value"]
+with_intercept = args["with-intercept"]
+force_intercept = args["force-intercept"]
+
+symmetric = args["symmetric-choice"]
+
+if force_intercept
+    with_intercept = true
+end
 
 lag_from = args["lag-from"]
 lag_to = args["lag-to"]
 
 if lag_to === nothing
-    lag_to = min(35,size(df)[1] - 3 - apply_D_count)
+    lag_to = min(20,size(df)[1] - 3 - apply_D_count)
 end
 
 
+println("p: $p_value, fit_p: $fit_p_value, sym:$symmetric, ICPT:$with_intercept force?:$force_intercept, diff:$apply_D_count, lag=$lag_from:$lag_to")
 
 components = [col for col in names(df) if !(col == time_col)]
 
@@ -308,8 +442,10 @@ i_column_names = [Symbol("P[D^($apply_D_count)["*String(j_col)*"],j](k)") for j_
 
 output = Dict{Symbol,Array{Any,1}}(key => Real[] for key in i_column_names)
 
-stats = Dict{Symbol,Array{Real,1}}(key => Real[] for key in [Symbol("lag"),Symbol("X(lag)"),
-                            Symbol("X(lag) with insignificant entries")]) #,Symbol("XR(lag)")])
+stats = Dict{Symbol,Array{Any,1}}(key => Real[] for key in [Symbol("lag"),Symbol("X(lag)"),Symbol("#Coeff-U"),
+Symbol("#Coeff-V"),Symbol("p-value")]) #,Symbol("XR(lag)")])
+
+stats[Symbol("significant?")] = String[]
 
 output[lag_column] = Int[]
 output[j_column] = String[]
@@ -319,17 +455,18 @@ n_columns, = size(column_names)
 
 n_obs, = size(diff_components)
 
+chi_dist = Chisq(length(components)^2)
+
 for lag_k = lag_from:lag_to
     global P,X,U,V,CVU,DU,DV,P0
-    println("\n partial cross-correlation matrix for lag = $lag_k")
-    U,V = calculate_residual_vectors(diff_components,lag_k)
+    print("\rpartial cross-correlation matrix for lag = $lag_k")
+    U,V,nU,nV = calculate_residual_vectors(diff_components,lag_k,fit_p_value,with_intercept,force_intercept,symmetric)
     CVU = C_UV(V,U)
     DU = C_UV(U,U,true)
     DV = C_UV(V,V,true)
     
     #R = sample_cor_matrix(U,V)
-    P0 = DV * CVU * DU
-    X0 = sum(P0.^2)*(n_obs)
+    
     
     P = DV * CVU * DU
     for j in 1:n_columns
@@ -340,15 +477,25 @@ for lag_k = lag_from:lag_to
         end
     end
     
-    
     # calculate the test statistics 
-    X = sum(P.^2)*(n_obs-lag_k)
-    #XR = sum(R.^2)*(n_obs-lag_k)
+    X = sum(P.^2)*(n_obs-lag_k) # it says 'n' in the book, wonder whether that is nbr of total obs or used obs....
+    
     append!(stats[Symbol("lag")],[lag_k])
     append!(stats[Symbol("X(lag)")],[X])
-    append!(stats[Symbol("X(lag) with insignificant entries")],[X0])
+    append!(stats[Symbol("#Coeff-U")],[nU])
+    append!(stats[Symbol("#Coeff-V")],[nV])
+    X_pvalue = ccdf(chi_dist,X)
+    append!(stats[Symbol("p-value")],[X_pvalue])
+    if X_pvalue < p_value
+        append!(stats[Symbol("significant?")],["S"])
+    else
+        append!(stats[Symbol("significant?")],["."])
+    end
+    
+    
     #append!(stats[Symbol("XR(lag)")],[XR])
 end
+print("\r")
 
 df_stats = DataFrame(stats)
 
